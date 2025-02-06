1fdef58c11a51b947948c39120026335c2e5b124
nulledusername <lucho.jobly@gmail.com>
2025-02-05 23:52:04 -0300

    definicio types

--- apps/dashboard/components/DatasourceForms/types.ts
+++ apps/dashboard/components/DatasourceForms/types.ts
@@ -1,9 +1,10 @@
-// import type { LoadingButtonProps } from '@mui/lab/LoadingButton';
 import type { DatasourceSchema } from '@chaindesk/lib/types/models';
 import type { AppDatasource as Datasource } from '@chaindesk/prisma';
+import { ZodSchema } from "zod";  // Importamos Zod
 
 export type DatasourceFormProps<T extends {} = DatasourceSchema> = {
   defaultValues?: T;
+  schema?: ZodSchema<any>; // <-- Agregado para evitar el error
   customSubmitButton?: any;
   submitButtonText?: string;
   submitButtonProps?: any;
