73db1fba0b3cbc27140a2c53de7a3b55c11af25b
nulledusername <lucho.jobly@gmail.com>
2025-02-11 11:45:56 -0300

    scouts

--- apps/dashboard/pages/agents/index.tsx
+++ apps/dashboard/pages/agents/index.tsx
@@ -106,7 +106,7 @@ export default function AgentsPage() {
           <HomeRoundedIcon />
         </Link>
         <Typography fontSize="inherit" color="neutral">
-          Agents
+          Crear Scout
         </Typography>
       </Breadcrumbs>
 
