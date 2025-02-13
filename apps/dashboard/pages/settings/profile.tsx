bf251167dba58b1b9859f33767e826d3fb31fd1c
nulledusername <lucho.jobly@gmail.com>
2025-02-13 18:08:17 -0300

    backup

--- apps/dashboard/pages/settings/profile.tsx
+++ apps/dashboard/pages/settings/profile.tsx
@@ -1,396 +1,174 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import CorporateFareRoundedIcon from '@mui/icons-material/CorporateFareRounded';
-import DeleteIcon from '@mui/icons-material/Delete';
 import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import Chip from '@mui/joy/Chip';
-import CircularProgress from '@mui/joy/CircularProgress';
 import Divider from '@mui/joy/Divider';
-import IconButton from '@mui/joy/IconButton';
-import Sheet from '@mui/joy/Sheet';
 import Stack from '@mui/joy/Stack';
-import Table from '@mui/joy/Table';
-import Typography from '@mui/joy/Typography';
 import axios from 'axios';
+import cuid from 'cuid';
 import mime from 'mime-types';
+import { GetServerSidePropsContext } from 'next/types';
 import { useSession } from 'next-auth/react';
-import React, { useEffect, useMemo } from 'react';
+import { ReactElement } from 'react';
+import * as React from 'react';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
-import useSWR, { mutate } from 'swr';
 
-import { getMemberships } from '@app/pages/api/memberships';
-import { getOrg } from '@app/pages/api/organizations/[id]';
+import OrganizationForm from '@app/components/OrganizationForm';
+import SettingsLayout from '@app/components/SettingsLayout';
+import IconInput from '@app/components/ui/IconInput';
+import SettingCard from '@app/components/ui/SettingCard';
 
-import accountConfig from '@chaindesk/lib/account-config';
-import { ApiErrorType } from '@chaindesk/lib/api-error';
-import getS3RootDomain from '@chaindesk/lib/get-s3-root-domain';
-import { hasAdminRole } from '@chaindesk/lib/has-oneof-roles';
-import { fetcher } from '@chaindesk/lib/swr-fetcher';
-import {
-  GenerateUploadLinkRequest,
-  OrganizationInviteSchema,
-  UpdateOrgSchema,
-} from '@chaindesk/lib/types/dtos';
-import { MembershipRole, Prisma } from '@chaindesk/prisma';
+import { UpdateUserProfileSchema } from '@chaindesk/lib/types/dtos';
+import { withAuth } from '@chaindesk/lib/withAuth';
+import useFileUpload from '@chaindesk/ui/hooks/useFileUpload';
 import useStateReducer from '@chaindesk/ui/hooks/useStateReducer';
+import Input from '@chaindesk/ui/Input';
 
-import IconInput from '@/ui/IconInput';
-import SettingCard from '@/ui/SettingCard';
-import Input from '@/Input';
-
-type Props = {};
-
-function OrganizationForm({}: Props) {
-  const { data: session } = useSession();
-  const methods = useForm<OrganizationInviteSchema>({
-    resolver: zodResolver(OrganizationInviteSchema),
-  });
-
-  const updateOrgMethods = useForm<UpdateOrgSchema>({
-    resolver: zodResolver(UpdateOrgSchema),
-  });
-
-  const getMembershipsQuery = useSWR<
-    Prisma.PromiseReturnType<typeof getMemberships>
-  >(`/api/memberships`, fetcher);
-
-  const getOrganizationQuery = useSWR<Prisma.PromiseReturnType<typeof getOrg>>(
-    session?.organization?.id
-      ? `/api/organizations/${session?.organization?.id}`
-      : null,
-    fetcher
-  );
-
+export default function ProfileSettingsPage() {
+  const { data: session, status, update } = useSession();
   const [state, setState] = useStateReducer({
-    isSubmitting: false,
-    isDeletingMember: false,
-    isUploadingIcon: false,
-    isDeletingIcon: false,
-    isUpdatingOrg: false,
-    iconUrl: getOrganizationQuery?.data?.iconUrl || '',
+    isUpdatingProfile: false,
+  });
+  const { upload, isUploading } = useFileUpload();
+
+  const methods = useForm<UpdateUserProfileSchema>({
+    resolver: zodResolver(UpdateUserProfileSchema),
   });
 
-  const submitInvite = async (values: OrganizationInviteSchema) => {
+  const handleUploadPicture = async (event: any) => {
+    const file = event?.target?.files?.[0];
+    const fileName = `${cuid()}.${mime.extension(file.type)}`;
+
+    const [customPictureUrl] = await upload([
+      {
+        case: 'userIcon',
+        fileName,
+        file,
+      },
+    ]);
+
+    methods.setValue('customPicture', customPictureUrl, {
+      shouldDirty: true,
+      shouldValidate: true,
+    });
+
+    await methods.handleSubmit(updateProfile)();
+  };
+
+  console.log('EROORS', methods.formState);
+
+  const updateProfile = async (data: UpdateUserProfileSchema) => {
     try {
-      if (
-        Number(getMembershipsQuery.data?.length) >=
-        accountConfig[session?.organization?.currentPlan!]?.limits?.maxSeats
-      ) {
-        alert(
-          'Has alcanzado el límite de miembros de tu plan. Por favor contacta a soporte@braviloai.com para actualizar tu plan.'
-        );
-        return;
-      }
-
       setState({
-        isSubmitting: true,
+        isUpdatingProfile: true,
       });
-      await axios.post(`/api/organizations/invite`, values);
 
-      await getMembershipsQuery.mutate();
+      await axios.patch('/api/accounts/profile', data);
 
-      methods.reset();
+      await update();
+
+      toast.success('Profile updated successfully.');
     } catch (err) {
       console.error(err);
-
-      if (
-        (err as any)?.response?.data?.error === ApiErrorType.ALREADY_INVITED
-      ) {
-        toast.error('Usuario ya invitado', {
-          duration: 5000,
-        });
-
-        methods.setError('email', {
-          message: 'Usuario ya invitado',
-        });
-      } else {
-        toast.error(
-          "Error al enviar invitación. Por favor contacta a soporte@braviloai.com si el problema persiste.",
-          {
-            duration: 5000,
-          }
-        );
-      }
+      toast.success('Error updating profile.');
     } finally {
       setState({
-        isSubmitting: false,
+        isUpdatingProfile: false,
       });
     }
   };
 
-  const handleDeleteMember = async (id: string) => {
-    try {
-      if (
-        window.confirm(
-          '¿Estás seguro de eliminar este miembro del equipo?'
-        )
-      ) {
-        setState({
-          isDeletingMember: true,
-        });
+  React.useEffect(() => {
+    if (session?.user?.email) {
+      methods.setValue('email', session?.user?.email);
+    }
+  }, [session?.user?.email]);
 
-        await axios.delete(`/api/memberships/${id}`);
-
-        await getMembershipsQuery.mutate();
-      }
-    } catch {
-    } finally {
-      setState({
-        isDeletingMember: false,
+  React.useEffect(() => {
+    if (status === 'authenticated') {
+      methods.reset({
+        email: session?.user?.email || '',
+        name: session?.user?.name || '',
+        customPicture: session?.user?.customPicture,
       });
     }
-  };
+  }, [status]);
 
-  const handleUpdateOrg = async (values: UpdateOrgSchema) => {
-    try {
-      setState({
-        isUpdatingOrg: true,
-      });
-      await axios.patch(`/api/organizations/${session?.organization?.id}`, {
-        ...values,
-      });
-
-      await getOrganizationQuery.mutate();
-
-      await mutate('/api/organizations');
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setState({
-        isUpdatingOrg: false,
-      });
-    }
-  };
-
-  const handleUploadIcon = async (event: any) => {
-    try {
-      setState({ isUpdatingOrg: true });
-      const file = event.target.files[0];
-      const fileName = `icon.${mime.extension(file.type)}`;
-
-      const uploadLinkRes = await axios.post(
-        `/api/organizations/${session?.organization?.id}/generate-upload-link`,
-        {
-          fileName,
-          type: file.type,
-        } as GenerateUploadLinkRequest
-      );
-
-      await axios.put(uploadLinkRes.data, file, {
-        headers: {
-          'Content-Type': file.type,
-        },
-      });
-
-      const iconUrl = `${getS3RootDomain()}/organizations/${
-        session?.organization?.id
-      }/${fileName}`;
-
-      setState({
-        iconUrl: iconUrl,
-      });
-
-      updateOrgMethods.setValue('iconUrl', iconUrl);
-
-      await handleUpdateOrg({
-        ...updateOrgMethods.getValues(),
-        iconUrl,
-      });
-
-      toast.success('¡Icono actualizado correctamente!');
-    } catch (err) {
-      console.log(err, err);
-    } finally {
-      setState({ isUpdatingOrg: false });
-    }
-  };
-
-  const handleDeleteIcon = async () => {
-    try {
-      setState({
-        isUpdatingOrg: true,
-      });
-
-      await handleUpdateOrg({
-        ...updateOrgMethods.getValues(),
-        iconUrl: null,
-      });
-
-      await getOrganizationQuery.mutate();
-
-      setState({ iconUrl: '' });
-
-      toast.success('¡Icono eliminado correctamente!');
-    } catch (err) {
-    } finally {
-      setState({
-        isUpdatingOrg: false,
-      });
-    }
-  };
-
-  useEffect(() => {
-    updateOrgMethods.reset({
-      name: getOrganizationQuery?.data?.name,
-      iconUrl: getOrganizationQuery?.data?.iconUrl,
-    });
-  }, [getOrganizationQuery?.data]);
-
-  if (!getOrganizationQuery?.data) {
+  if (!session?.user) {
     return null;
   }
 
+  const customPicture = methods.watch('customPicture');
+
   return (
-    <Stack gap={4} id="team">
+    <Stack
+      sx={{
+        height: '100%',
+      }}
+    >
+      {/* <OrganizationForm /> */}
+
       <SettingCard
-        title="Configuración del Equipo"
-        description="Personaliza el nombre y el icono de tu equipo"
-        disableSubmitButton
+        title="Profile Settings"
+        description="Your personal information and settings."
+        cardProps={{
+          sx: { maxWidth: 'md', mx: 'auto', width: '100%' },
+        }}
+        submitButtonProps={{
+          loading: state.isUpdatingProfile,
+          disabled: !methods.formState?.isValid || !methods?.formState?.isDirty,
+          children: 'Update',
+          onClick: () => updateProfile(methods.getValues()),
+        }}
       >
         <form
-          className="flex flex-col"
-          onSubmit={updateOrgMethods.handleSubmit(handleUpdateOrg)}
+          className="space-y-4"
+          onSubmit={methods.handleSubmit(updateProfile)}
         >
           <Input
-            control={updateOrgMethods.control}
-            value={getOrganizationQuery?.data?.iconUrl! || ''}
-            hidden
-            sx={{ height: 0 }}
-            {...updateOrgMethods.register('iconUrl')}
+            control={methods.control}
+            label="Email"
+            value={session?.user?.email || ''}
+            disabled
+            {...methods.register('email')}
           />
 
-          <Stack gap={2}>
-            <IconInput
-              innerIcon={<CorporateFareRoundedIcon />}
-              defaultIconUrl={getOrganizationQuery?.data?.iconUrl!}
-              value={
-                getOrganizationQuery?.data?.iconUrl
-                  ? `${
-                      getOrganizationQuery?.data?.iconUrl
-                    }?timestamp=${Date.now()}`
-                  : ''
-              }
-              onChange={handleUploadIcon}
-              onDelete={handleDeleteIcon}
-              disabled={!hasAdminRole(session?.roles)}
-              loading={state.isUpdatingOrg}
-            />
-            <Input
-              control={updateOrgMethods.control}
-              label="Nombre del Equipo"
-              defaultValue={getOrganizationQuery?.data?.name!}
-              disabled={!hasAdminRole(session?.roles) || state.isUpdatingOrg}
-              {...updateOrgMethods.register('name')}
-            />
+          <Input
+            control={methods.control}
+            label="Name"
+            defaultValue={session?.user?.name || ''}
+            {...methods.register('name')}
+          />
 
-            <Button
-              type="submit"
-              size="sm"
-              sx={{ ml: 'auto' }}
-              disabled={!hasAdminRole(session?.roles)}
-              loading={state.isUpdatingOrg}
-            >
-              Actualizar
-            </Button>
-          </Stack>
+          <IconInput
+            // innerIcon={<CorporateFareRoundedIcon />}
+            defaultIconUrl={session?.user?.customPicture || ''}
+            value={customPicture!}
+            onChange={handleUploadPicture}
+            onDelete={() => {
+              methods.setValue('customPicture', null, {
+                shouldDirty: true,
+                shouldValidate: true,
+              });
+            }}
+            // disabled={!hasAdminRole(session?.roles)}
+            loading={isUploading || methods.formState.isSubmitting}
+          />
+
+          <input type="submit" hidden />
         </form>
-      </SettingCard>
-
-      <SettingCard
-        title="Miembros del Equipo"
-        description="Invita nuevos miembros a colaborar"
-        disableSubmitButton
-      >
-        <form onSubmit={methods.handleSubmit(submitInvite)} className="p-2">
-          <Stack gap={2}>
-            <Input
-              control={methods.control}
-              {...methods.register('email')}
-              label="Correo electrónico"
-              onBlur={(e) => {}}
-              endDecorator={
-                <Button type="submit" loading={state.isSubmitting}>
-                  Invitar
-                </Button>
-              }
-            />
-          </Stack>
-        </form>
-
-        {Number(getMembershipsQuery?.data?.length) > 0 && (
-          <Stack sx={{ px: 2 }} gap={1}>
-            <Typography
-              level="body-md"
-              fontWeight={'bold'}
-              color={
-                Number(getMembershipsQuery?.data?.length) >=
-                accountConfig[session?.organization?.currentPlan!]?.limits
-                  ?.maxSeats
-                  ? 'danger'
-                  : 'success'
-              }
-            >{`${getMembershipsQuery?.data?.length}/${
-              accountConfig[session?.organization?.currentPlan!]?.limits
-                ?.maxSeats
-            } plazas utilizadas`}</Typography>
-            <Sheet sx={{ borderRadius: 'md' }}>
-              <Table variant="outlined">
-                <thead>
-                  <tr>
-                    <th style={{ width: '40%' }}>Correo</th>
-                    <th>Rol</th>
-                    <th>Estado</th>
-                    <th>Acciones</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {getMembershipsQuery?.data?.map((member) => (
-                    <tr key={member?.id}>
-                      <td>{member?.invitedEmail || member?.user?.email}</td>
-                      <td>
-                        <Chip size="sm" variant="soft" color="neutral">
-                          {member?.role}
-                        </Chip>
-                      </td>
-                      <td>
-                        <Chip
-                          size={'sm'}
-                          variant="soft"
-                          color={member?.userId ? 'success' : 'warning'}
-                        >
-                          {member?.userId ? 'Activo' : 'Pendiente'}
-                        </Chip>
-                      </td>
-                      <td>
-                        <IconButton
-                          size="sm"
-                          color="danger"
-                          variant="plain"
-                          onClick={() => handleDeleteMember(member?.id)}
-                          disabled={
-                            state.isDeletingMember ||
-                            member.role === MembershipRole.OWNER ||
-                            !hasAdminRole(session?.roles)
-                          }
-                        >
-                          {state.isDeletingMember ? (
-                            <CircularProgress size="sm" />
-                          ) : (
-                            <DeleteIcon />
-                          )}
-                        </IconButton>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Sheet>
-          </Stack>
-        )}
       </SettingCard>
     </Stack>
   );
 }
 
-export default OrganizationForm;
+ProfileSettingsPage.getLayout = function getLayout(page: ReactElement) {
+  return <SettingsLayout>{page}</SettingsLayout>;
+};
+
+// export const getServerSideProps = withAuth(
+//   async (ctx: GetServerSidePropsContext) => {
+//     return {
+//       props: {},
+//     };
+//   }
+// );
