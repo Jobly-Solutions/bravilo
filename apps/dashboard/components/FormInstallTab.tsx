3f46aa6bf069dfb073be954f24868aa88203154b
nulledusername <lucho.jobly@gmail.com>
2025-02-13 17:23:35 -0300

    install form

--- apps/dashboard/components/FormInstallTab.tsx
+++ apps/dashboard/components/FormInstallTab.tsx
@@ -31,32 +31,48 @@ function FormInstallTab({ formId }: Props) {
 ></iframe>
 `;
 
-  return (
-    <Card sx={{ mx: 'auto' }}>
-      <AccordionGroup size="lg">
-        <Accordion defaultExpanded>
-          <AccordionSummary>Web Component</AccordionSummary>
-          <AccordionDetails>
-            <Markdown>{`~~~html\n ${installScript} \n~~~`}</Markdown>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary>iFrame</AccordionSummary>
-          <AccordionDetails>
-            <Markdown>{`~~~html\n ${installScriptIframe} \n~~~`}</Markdown>
-          </AccordionDetails>
-        </Accordion>
+return (
+  <Card sx={{ mx: 'auto' }}>
+    <AccordionGroup size="lg">
+      {/* Componente Web - Integración directa */}
+      <Accordion defaultExpanded>
+        <AccordionSummary>Componente Web</AccordionSummary>
+        <AccordionDetails>
+          <Markdown>{`~~~html\n ${installScript} \n~~~`}</Markdown>
+          <Typography level="body-sm" sx={{ mt: 2 }}>
+            {/* Explicación */}
+            Este código permite integrar el formulario directamente en tu sitio web como un componente nativo. 
+            Ideal para una integración perfecta con tu diseño existente. Simplemente copia y pega en el HTML 
+            donde quieres que aparezca el formulario.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
 
-        {/* <Accordion>
-          <AccordionSummary>Third accordion</AccordionSummary>
-          <AccordionDetails>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </AccordionDetails>
-        </Accordion> */}
-      </AccordionGroup>
-    </Card>
-  );
+      {/* iFrame - Integración universal */}
+      <Accordion>
+        <AccordionSummary>iFrame</AccordionSummary>
+        <AccordionDetails>
+          <Markdown>{`~~~html\n ${installScriptIframe} \n~~~`}</Markdown>
+          <Typography level="body-sm" sx={{ mt: 2 }}>
+            {/* Explicación */}
+            Usa esta opción para incrustar el formulario en cualquier plataforma que permita iframes. 
+            Funciona en CMS como WordPress, Shopify o sistemas legacy. El formulario se cargará de forma 
+            independiente manteniendo su funcionalidad completa.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* Ejemplo de sección adicional (deshabilitada) */}
+      {/* <Accordion>
+        <AccordionSummary>Tercer método</AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </AccordionDetails>
+      </Accordion> */}
+    </AccordionGroup>
+  </Card>
+);
 }
 
 export default FormInstallTab;
